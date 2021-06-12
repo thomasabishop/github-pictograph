d0f0476f58c13894d3635063ff63b1e02307c8b2
thomasabishop <tactonbishop@gmail.com>
2021-06-12 14:52:51 +0100

    Make search form responsive mob

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const SearchForm: React.FunctionComponent = () => (
-  <>
-    <Form className="pt-4">
-      <Row>
-        <Col xs={8}>
-          <Form.Control placeholder="User name" />
-        </Col>
-        <Col xs={4}>
-          <Button variant="outline-primary" type="submit">
-            Submit
-          </Button>
-          <Button className="ml-3" variant="outline-danger" type="submit">
-            Reset
-          </Button>
-        </Col>
-      </Row>
-    </Form>
-  </>
-)
-
+const SearchForm: React.FunctionComponent = () => {
+  const [contributor, setContributor] = useState()
+  return (
+    <>
+      <Form className="pt-4">
+        <Row>
+          <Col xs={12} sm={8}>
+            <Form.Control placeholder="User name" size="sm" />
+          </Col>
+          <Col xs={12} sm={4} className="mt-2 mt-sm-0">
+            <Button variant="outline-primary" type="submit" size="sm">
+              Submit
+            </Button>
+            <Button
+              className="ml-3"
+              variant="outline-danger"
+              type="submit"
+              size="sm"
+            >
+              Reset
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  )
+}
 export default SearchForm
