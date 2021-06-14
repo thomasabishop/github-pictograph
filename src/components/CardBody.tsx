6feae238dd1c1bbc4dff4534040e14eb889b04ca
thomasabishop <tactonbishop@gmail.com>
2021-06-14 22:09:35 +0100

    Add sparkline, pictograph SCSS

--- src/components/CardBody.tsx
+++ src/components/CardBody.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { Sparklines, SparklinesLine } from 'react-sparklines'
+import Pictograph from './Pictograph'
+
+const ContributorSparkline: React.FunctionComponent = () => (
+  <Sparklines
+    data={[5, 10, 5, 20, 5, 10]}
+    limit={6}
+    width={40}
+    height={20}
+    margin={5}
+  >
+    <SparklinesLine color="blue" />
+  </Sparklines>
+)
 
 const CardBody: React.FunctionComponent = () => (
   <div className="DataCard--body">
-    <div>
-      <p>183 contributions in the last 90 days</p>
+    <div className="DataCard--summary">
+      <div>
+        <p>183 contributions in the last 90 days</p>
+      </div>
+      <div className="Sparkline">
+        <ContributorSparkline />
+      </div>
     </div>
-    <div>
-      <p>SparkLine</p>
-    </div>
+    <Pictograph />
   </div>
 )
 
---
+++ src/components/Pictograph.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pictograph: React.FunctionComponent = () => (
+  <div className="Pictograph">
+    <div className="year">
+      <div className="week">
+        <div></div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Pictograph
