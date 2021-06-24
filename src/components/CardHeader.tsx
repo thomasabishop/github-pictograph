768e9d55e323dc1ebd349094b3cfbe9efe008081
thomasabishop <tactonbishop@gmail.com>
2021-06-24 22:02:56 +0100

    Add props to component for username and user-image

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import thomas from '../images/thomas.png'
 
-const CardHeader: React.FunctionComponent = () => (
-  <div className="DataCard--header">
-    <div>
-      <img src={thomas} alt="Contributor" />
-    </div>
-    <div className="DataCard--header_name">
-      <h5>@thomasabishop</h5>
-    </div>
-  </div>
-)
+// -> I define a type for the props local to the file not in .d.ts so I can retain casing convention and promote clarity
+type Props = {
+  name: string
+  avatar: string
+}
 
+// -> This component takes props so don't use React.FC : just causes unnecessary aggro with TS compiler
+const CardHeader = (props: Props): JSX.Element => {
+  //  Destructure the props first
+  const { name, avatar } = props
+  return (
+    <div className="DataCard--header">
+      <div>
+        <img
+          className="DataCard--header_avatar"
+          src={avatar}
+          alt="Contributor"
+        />
+      </div>
+      <div className="DataCard--header_name">
+        <h5>@{name}</h5>
+      </div>
+    </div>
+  )
+}
 export default CardHeader
