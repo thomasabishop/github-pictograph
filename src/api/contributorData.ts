3f50badd6b6cf08cbf6f2f6dbc4414923037124b
thomasabishop <tactonbishop@gmail.com>
2021-06-24 22:14:43 +0100

    Resolve TS compiler bug caused by contributorDetails returning union type of string and Promise

--- src/api/contributorData.ts
+++ src/api/contributorData.ts
@@ -2,14 +2,12 @@ import { Octokit } from 'octokit'
 import { Endpoints } from '@octokit/types'
 import { ghToken } from '../credentials'
 
-type TContributor = {
-  username: string | undefined
-  userImage?: string | undefined
-}
-
 type userParameters = Endpoints['GET /users/{username}']['parameters']
-
 type userResponse = Endpoints['GET /users/{username}']['response']
+
+/**
+ * Generic query of GH User API
+ */
 
 async function queryUserApi(
   options: userParameters
@@ -25,18 +23,26 @@ async function queryUserApi(
   throw new Error('Something else went wrong')
 }
 
+/**
+ * Match user-inputted name to User database. If valid, return validated GH username and interpolate user image URL from this
+ */
+
+// TO-DO:
+// Link this function to error handling of form input
 export async function contributorDetails(
   contributorName: string
-): Promise<TContributor | string> {
-  const checkUser = await queryUserApi({ username: contributorName })
-  if (checkUser.login === contributorName) {
-    const contributor: TContributor = {
-      username: checkUser.login,
-      userImage: `https://github.com/${contributorName}.png`,
+): Promise<TContributor> {
+  try {
+    const checkUser = await queryUserApi({ username: contributorName })
+    if (checkUser.login === contributorName) {
+      const contributor: TContributor = {
+        username: checkUser.login,
+        userImage: `https://github.com/${contributorName}.png`,
+      }
+      return contributor
     }
-    return contributor
+  } catch (error) {
+    console.error(error) //eslint-disable-line
   }
-  return 'User could not be found'
+  throw new Error('User could not be found')
 }
-
-console.log(contributorDetails('thomasabishop'))
