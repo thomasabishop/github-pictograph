5be4168c1026149e190afd84fcc935f4e9e4c0e1
thomasabishop <tactonbishop@gmail.com>
2021-06-24 22:15:23 +0100

    Add state handling and pass props to revised CardHeader component

--- src/components/DataCard.tsx
+++ src/components/DataCard.tsx
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react'
-import Sparkline from './Sparkline'
 import { useContributor } from '../state/ContributorContext'
+import { contributorDetails } from '../api/contributorData'
 import CardBody from './CardBody'
 import CardHeader from './CardHeader'
+import blankAvatar from '../images/blank-avatar.png'
+
+const startData: TContributor = {
+  username: 'Contributor',
+  userImage: blankAvatar,
+}
 
 const DataCard: React.FunctionComponent = () => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const { contributor } = useContributor()!
-  const [info, setInfo] = useState<any>()
+  const [contributorData, setContributorData] =
+    useState<TContributor>(startData)
+
   useEffect(() => {
     if (contributor) {
-      //
+      contributorDetails(contributor).then((response) => {
+        setContributorData(response)
+      })
     }
   }, [contributor])
-
   return (
     <>
       <div className="DataCard">
-        <CardHeader />
+        <CardHeader
+          name={contributorData.username}
+          avatar={contributorData.userImage}
+        />
         <CardBody />
       </div>
     </>
