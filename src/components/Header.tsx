62fd36a77b9727a28bda50e657535b4db2577e3f
thomasabishop <tactonbishop@gmail.com>
2021-06-25 17:43:20 +0100

    Change header icon and background color, add nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import octocat from '../images/octokat.png'
+import { Nav, Navbar } from 'react-bootstrap'
+import octocat from '../images/github-logo-square.svg'
+
 const Header: React.FunctionComponent = () => (
   <>
-    <Navbar variant="light" bg="light">
+    <Navbar variant="dark" bg="primary">
       <Navbar.Brand href="#home">
         <img
           alt=""
-          width="40"
+          width="30"
           src={octocat}
           className="d-inline-block align-top mr-2"
         />
-        GitHub Contributors Recreation
+        GitHub Contributions
       </Navbar.Brand>
+      <Nav className="justify-content-end">
+        <Nav.Link href="#home">Source</Nav.Link>
+      </Nav>
     </Navbar>
   </>
 )
