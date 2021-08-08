011e436d917e8a97ae44e26fb54f622b27481805
thomasabishop <tactonbishop@gmail.com>
2021-08-08 15:36:26 +0100

    Add coments and contributor freq request to getContributorDetails

--- src/functions/getContributorData.ts
+++ src/functions/getContributorData.ts
@@ -1,14 +1,15 @@
-import { IContributor } from '../model/IContributor'
-import queryUserIdentity from '../api/queryUserIdentity'
-import queryUserEvents from '../api/queryUserEvents'
-import getContributionFrequencies from './getContributionFrequencies'
 /**
  * Match user-inputted name to User database. If valid, return validated GH username and interpolate user image URL from this
+ * @param username GitHub username entered by user (not yet validated)
  */
 
 // TO-DO:
 // Link this function to error handling of form input
 
+import { IContributor } from '../model/IContributor'
+import queryUserIdentity from '../api/queryUserIdentity'
+import queryUserEvents from '../api/queryUserEvents'
+import getContributionFrequencies from './getContributionFrequencies'
 export async function getContributorData(
   contributorName: string
 ): Promise<IContributor> {
@@ -19,13 +20,28 @@ export async function getContributorData(
     const contributorEvents = await queryUserEvents({
       username: contributorName,
     })
-    console.log(contributorEvents)
+
     const contributorFreqs = await getContributionFrequencies(contributorName)
-    console.log(contributorFreqs)
+
+    // console.group('contributorDetails:')
+    // console.log(contributorDetails)
+    // console.groupEnd()
+
+    // console.group('contributorEvents:')
+    // console.log(contributorEvents)
+    // console.groupEnd()
+
+    // console.group('contributorFrequencies:')
+    // console.log(contributorFreqs)
+    // console.groupEnd()
+
+    // Check username corresponds to actual GitHub contributor
     if (contributorDetails.login === contributorName) {
-      const contributor: TContributor = {
+      // If so set fields for contributor using above API requests
+      const contributor: IContributor = {
         username: contributorDetails.login,
         userImage: `https://github.com/${contributorName}.png`,
+        contributions: contributorFreqs,
       }
       return contributor
     }
